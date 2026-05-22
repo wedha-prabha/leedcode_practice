83e94f0a0f460bc9e07ba2e38b1a9c54e35caa20
sameeraa31 <sameera.r.iot.2023@snsce.ac.in>
2026-05-22 17:57:24 +0530

    Added Two Sum solution using hash map

---
+++ array/two_sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums, target):
+        num_map = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[nums[i]] = i
+nums = [2, 7, 11, 15]
+target = 9
+solution = Solution()
+print(solution.twoSum(nums, target))
