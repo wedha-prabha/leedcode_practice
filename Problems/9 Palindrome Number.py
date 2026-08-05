4ac5c26ac02d286b9ce2aa1c48ba0eecda121d15
Wedha Prabha <wedhaprabha@gmail.com>
2026-08-05 15:08:00 +0530

    9. Palindrome Number

---
+++ Problems/9 Palindrome Number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        if x<0:
+            return False
+        reverse=0
+        xcopy=x
+        while x>0:
+            reverse=(reverse*10)+(x%10)
+            x//=10
+        return reverse ==xcopy
+        
