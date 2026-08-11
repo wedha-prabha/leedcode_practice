93e992c9429456f1fad05c94edb76efab7aafea4
Wedha Prabha <wedhaprabha@gmail.com>
2026-08-11 12:31:40 +0530

    49. Group Anagrams

---
+++ array/49 Group Anagrams.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        groups = {}
+        for word in strs:
+            key = ''.join(sorted(word))
+            if key not in groups:
+                groups[key] = []
+            groups[key].append(word)
+        return list(groups.values())
